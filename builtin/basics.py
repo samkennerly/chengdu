348ba09144f2f34dc1d1e400eb579f21f0a8102d
Sam Kennerly <samkennerly@gmail.com>
2026-03-29 20:06:56 +0800

    more textbook examples

--- builtin/basics.py
+++ builtin/basics.py
@@ -7,20 +7,57 @@ this might help to refresh your memory.
 print("Hello, world!")
 
 # Do some mathematics
-print("10 + 4 is", 10 + 4)
-print("10 * 4 is", 10 * 4)
-print("10 ** 4 is", 10 ** 4)
+print("10 + 4 =", 10 + 4)
+print("10 * 4 =", 10 * 4)
+print("10 ** 4 =", 10 ** 4)
 
 # Concatenate strings
-print("'Alice' + 'Bob' is", 'Alice' + 'Bob')
+print("'Alice' + 'Bob' =", 'Alice' + 'Bob')
 
 # Replicate a string
-print("5 * 'Alice' is", 5 * 'Alice')
+print("5 * 'Alice' =", 5 * 'Alice')
 
-# Try something which is not allowed
+# Try an illegal operation
 try:
     3.14159 * "Alice"
 except Exception as err:
     print(err)
+
+# Create some variables with silly names
+spam = 40
+print("spam =", spam)
+eggs = 2
+print("eggs =", eggs)
+
+# Do some mathematics with variables
+print("spam + eggs =", spam + eggs)
+print("spam + 2 * eggs =", spam + 2 * eggs)
+
+# Mutate spam
+spam = spam + 2
+print("spam = ", spam)
+spam = spam + 2
+print("spam = ", spam)
+spam += 2
+print("spam = ", spam)
+
+# Overwite spam
+spam = 'Hello'
+print("spam =", spam)
+spam = "你好"
+print("spam =", spam)
+
+# These are illegal variable names:
+#88888888 = "lucky"
+#8eights = "lucky"
+#eight8's = "lucky"
+#return = "lucky"
+
+
+
+
+
+
+
 
 
