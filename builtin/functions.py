ed200a9d1c52c1187af9ab1bd6bf2573d7afa5d2
Sam Kennerly <samkennerly@gmail.com>
2026-04-02 15:38:48 +0800

    add kwargs to functions.py

--- builtin/functions.py
+++ builtin/functions.py
@@ -52,3 +52,49 @@ print("The mean of 1.5 and 2.5 is", mean(1.5, 2.5))
 # If we input the wrong types, Python will still try to call the
 # mean() function. It will raise an error and crash.
 # print("The mean of '一' and '三' is", mean("一", "三"))
+
+
+# Python functions always return something.
+# If we write a function with no 'return' statement,
+# then Python will assume we meant 'return None'.
+
+z = mean(0, 100)
+print("mean(0, 100) returned", z)
+
+s = say_hello_to("Sam")
+print("say_hello_to('Sam') returned", s)
+
+
+# None is a special object with its own type.
+# It's an example of a 'null marker'.
+
+print("type(None) is", type(None))
+
+
+# Some functions accept 'named parameters' as input.
+# I call these 'keyword arguments' or 'kwargs'.
+# Kwargs are often used for optional inputs.
+# In this example, 'name' is a kwarg with default value None.
+
+def say_goodbye(name=None):
+    if name is None:
+        print("再见")
+    else:
+        print("再见" + name)
+
+say_goodbye(name = "Ada Lovelace")
+say_goodbye(name = "Charles Babbage")
+say_goodbye()
+
+
+# Many builtin functions accept kwargs. For example, print()
+# accepts a kwarg called 'sep'. It means 'separator'.
+# If you print many things, Python will insert a separator between
+# those things. By default, 'sep' is a space. We can change it:
+
+print("Ada", "Charles", "Guido")
+print("Ada", "Charles", "Guido", sep=" & ")
+
+
+
+
